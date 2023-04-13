005fdd9b9022b83df02cb38e81edcd8be4ae6a09
IvanStepanok <128456094+IvanStepanok@users.noreply.github.com>
2023-04-13 13:35:05 +0300

    add stage env (#15)

--- NewEdX/Environment.swift
+++ NewEdX/Environment.swift
@@ -11,6 +11,9 @@ enum `Environment`: String {
     case debugDev = "DebugDev"
     case releaseDev = "ReleaseDev"
     
+    case debugStage = "DebugStage"
+    case releaseStage = "ReleaseStage"
+    
     case debugProd = "DebugProd"
     case releaseProd = "ReleaseProd"
 }
@@ -24,6 +27,8 @@ class BuildConfiguration {
         switch environment {
         case .debugDev, .releaseDev:
             return "https://example-dev.com"
+        case .debugStage, .releaseStage:
+            return "https://example-stage.com"
         case .debugProd, .releaseProd:
             return "https://example.com"
         }
@@ -32,7 +37,9 @@ class BuildConfiguration {
     var clientId: String {
         switch environment {
         case .debugDev, .releaseDev:
-            return "DEBUG_CLIENT_ID"
+            return "DEV_CLIENT_ID"
+        case .debugStage, .releaseStage:
+            return "STAGE_CLIENT_ID"
         case .debugProd, .releaseProd:
             return "PROD_CLIENT_ID"
         }
