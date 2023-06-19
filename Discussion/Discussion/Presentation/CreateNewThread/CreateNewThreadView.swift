3d7778c546368158cabf0b0525201968e96af3a3
Ivan Stepanok <ivan.stepanok@raccoongang.com>
2023-06-19 17:01:52 +0300

    fix scrollAvoidKeyboard on CreateNewThreadView

--- Discussion/Discussion/Presentation/CreateNewThread/CreateNewThreadView.swift
+++ Discussion/Discussion/Presentation/CreateNewThread/CreateNewThreadView.swift
@@ -55,136 +55,138 @@ public struct CreateNewThreadView: View {
                             .padding(20)
                     }.frame(maxWidth: .infinity, maxHeight: .infinity)
                 } else {
-                    VStack(alignment: .leading) {
-                        HStack {
-                            Text(DiscussionLocalization.CreateThread.selectPostType)
-                                .font(Theme.Fonts.titleMedium)
-                                .foregroundColor(CoreAssets.textPrimary.swiftUIColor)
-                                .padding(.top, 32)
-                            Spacer()
-                        }
-                        
-                        Picker("", selection: $postType) {
-                            ForEach(postTypes, id: \.self) {
-                                Text($0.localizedValue.capitalized)
+                    ScrollView {
+                        VStack(alignment: .leading) {
+                            HStack {
+                                Text(DiscussionLocalization.CreateThread.selectPostType)
+                                    .font(Theme.Fonts.titleMedium)
+                                    .foregroundColor(CoreAssets.textPrimary.swiftUIColor)
+                                    .padding(.top, 32)
+                                Spacer()
                             }
-                        }.pickerStyle(.segmented)
-                            .frame(maxWidth: .infinity, maxHeight: 40)
-                        
-                        // MARK: Topic picker
-                        Group {
-                            Text(DiscussionLocalization.CreateThread.topic)
-                                .font(Theme.Fonts.titleSmall)
-                                .padding(.top, 16)
                             
-                            Menu {
-                                Picker(selection: $viewModel.selectedTopic) {
-                                    ForEach(viewModel.allTopics, id: \.id) {
-                                        Text($0.name)
-                                            .tag($0.id)
-                                            .font(Theme.Fonts.labelLarge)
-                                    }
-                                } label: {}
-                            } label: {
-                                HStack {
-                                    Text(viewModel.allTopics.first(where: {
-                                        $0.id == viewModel.selectedTopic })?.name ?? "")
-                                    .font(Theme.Fonts.labelLarge)
-                                    .frame(height: 40, alignment: .leading)
-                                    Spacer()
-                                    Image(systemName: "chevron.down")
-                                }.padding(.horizontal, 14)
-                                    .accentColor(CoreAssets.textPrimary.swiftUIColor)
-                                    .background(Theme.Shapes.textInputShape
-                                        .fill(CoreAssets.textInputBackground.swiftUIColor)
-                                    )
-                                    .overlay(
-                                        Theme.Shapes.textInputShape
-                                            .stroke(lineWidth: 1)
-                                            .fill(CoreAssets.textInputStroke.swiftUIColor)
-                                    )
-                            }
-                        }
-                        // MARK: End of topic picker
-                        
-                        Group {
-                            Text(DiscussionLocalization.CreateThread.title)
-                                .font(Theme.Fonts.titleSmall)
-                            + Text(" *").foregroundColor(.red)
-                        }.padding(.top, 16)
-                        TextField("", text: $postTitle)
-                            .font(Theme.Fonts.labelLarge)
-                            .padding(14)
-                            .frame(height: 40)
-                            .background(
-                                Theme.Shapes.textInputShape
-                                    .fill(CoreAssets.textInputBackground.swiftUIColor)
-                            )
-                            .overlay(
-                                Theme.Shapes.textInputShape
-                                    .stroke(lineWidth: 1)
-                                    .fill(
-                                        CoreAssets.textInputStroke.swiftUIColor
-                                    )
-                            )
-                        
-                        Group {
-                            Text("\(postType.localizedValue.capitalized)")
-                                .font(Theme.Fonts.titleSmall)
-                            + Text(" *").foregroundColor(.red)
-                        }.padding(.top, 16)
-                        TextEditor(text: $postBody)
-                            .font(Theme.Fonts.labelLarge)
-                            .padding(.horizontal, 10)
-                            .padding(.vertical, 10)
-                            .frame(height: 200)
-                            .hideScrollContentBackground()
-                            .background(
-                                Theme.Shapes.textInputShape
-                                    .fill(CoreAssets.textInputBackground.swiftUIColor)
-                            )
-                            .overlay(
-                                Theme.Shapes.textInputShape
-                                    .stroke(lineWidth: 1)
-                                    .fill(
-                                        CoreAssets.textInputStroke.swiftUIColor
-                                    )
-                            )
-                        
-                        CheckBoxView(checked: $followPost,
-                                     text: postType == .discussion
-                                     ? DiscussionLocalization.CreateThread.followDiscussion
-                                     : DiscussionLocalization.CreateThread.followQuestion
-                        )
-                        .padding(.top, 16)
-                        
-                        StyledButton(postType == .discussion
-                                     ? DiscussionLocalization.CreateThread.createDiscussion
-                                     : DiscussionLocalization.CreateThread.createQuestion, action: {
-                            if postTitle != "" && postBody != "" {
-                                let newThread = DiscussionNewThread(courseID: courseID,
-                                                                    topicID: viewModel.selectedTopic,
-                                                                    type: postType,
-                                                                    title: postTitle,
-                                                                    rawBody: postBody,
-                                                                    followPost: followPost)
-                                Task {
-                                    if await viewModel.createNewThread(newThread: newThread) {
-                                        onPostCreated()
-                                    }
+                            Picker("", selection: $postType) {
+                                ForEach(postTypes, id: \.self) {
+                                    Text($0.localizedValue.capitalized)
+                                }
+                            }.pickerStyle(.segmented)
+                                .frame(maxWidth: .infinity, maxHeight: 40)
+                            
+                            // MARK: Topic picker
+                            Group {
+                                Text(DiscussionLocalization.CreateThread.topic)
+                                    .font(Theme.Fonts.titleSmall)
+                                    .padding(.top, 16)
+                                
+                                Menu {
+                                    Picker(selection: $viewModel.selectedTopic) {
+                                        ForEach(viewModel.allTopics, id: \.id) {
+                                            Text($0.name)
+                                                .tag($0.id)
+                                                .font(Theme.Fonts.labelLarge)
+                                        }
+                                    } label: {}
+                                } label: {
+                                    HStack {
+                                        Text(viewModel.allTopics.first(where: {
+                                            $0.id == viewModel.selectedTopic })?.name ?? "")
+                                        .font(Theme.Fonts.labelLarge)
+                                        .frame(height: 40, alignment: .leading)
+                                        Spacer()
+                                        Image(systemName: "chevron.down")
+                                    }.padding(.horizontal, 14)
+                                        .accentColor(CoreAssets.textPrimary.swiftUIColor)
+                                        .background(Theme.Shapes.textInputShape
+                                            .fill(CoreAssets.textInputBackground.swiftUIColor)
+                                        )
+                                        .overlay(
+                                            Theme.Shapes.textInputShape
+                                                .stroke(lineWidth: 1)
+                                                .fill(CoreAssets.textInputStroke.swiftUIColor)
+                                        )
                                 }
                             }
-                        })
-                        .padding(.top, 26)
-                        .saturation(!postTitle.isEmpty && !postBody.isEmpty ? 1 : 0)
-                        Spacer()
-                    }.padding(.horizontal, 24)
-                        .frameLimit()
-                        .onRightSwipeGesture {
-                            viewModel.router.back()
-                        }
+                            // MARK: End of topic picker
+                            
+                            Group {
+                                Text(DiscussionLocalization.CreateThread.title)
+                                    .font(Theme.Fonts.titleSmall)
+                                + Text(" *").foregroundColor(.red)
+                            }.padding(.top, 16)
+                            TextField("", text: $postTitle)
+                                .font(Theme.Fonts.labelLarge)
+                                .padding(14)
+                                .frame(height: 40)
+                                .background(
+                                    Theme.Shapes.textInputShape
+                                        .fill(CoreAssets.textInputBackground.swiftUIColor)
+                                )
+                                .overlay(
+                                    Theme.Shapes.textInputShape
+                                        .stroke(lineWidth: 1)
+                                        .fill(
+                                            CoreAssets.textInputStroke.swiftUIColor
+                                        )
+                                )
+                            
+                            Group {
+                                Text("\(postType.localizedValue.capitalized)")
+                                    .font(Theme.Fonts.titleSmall)
+                                + Text(" *").foregroundColor(.red)
+                            }.padding(.top, 16)
+                            TextEditor(text: $postBody)
+                                .font(Theme.Fonts.labelLarge)
+                                .padding(.horizontal, 10)
+                                .padding(.vertical, 10)
+                                .frame(height: 200)
+                                .hideScrollContentBackground()
+                                .background(
+                                    Theme.Shapes.textInputShape
+                                        .fill(CoreAssets.textInputBackground.swiftUIColor)
+                                )
+                                .overlay(
+                                    Theme.Shapes.textInputShape
+                                        .stroke(lineWidth: 1)
+                                        .fill(
+                                            CoreAssets.textInputStroke.swiftUIColor
+                                        )
+                                )
+                            
+                            CheckBoxView(checked: $followPost,
+                                         text: postType == .discussion
+                                         ? DiscussionLocalization.CreateThread.followDiscussion
+                                         : DiscussionLocalization.CreateThread.followQuestion
+                            )
+                            .padding(.top, 16)
+                            
+                            StyledButton(postType == .discussion
+                                         ? DiscussionLocalization.CreateThread.createDiscussion
+                                         : DiscussionLocalization.CreateThread.createQuestion, action: {
+                                if postTitle != "" && postBody != "" {
+                                    let newThread = DiscussionNewThread(courseID: courseID,
+                                                                        topicID: viewModel.selectedTopic,
+                                                                        type: postType,
+                                                                        title: postTitle,
+                                                                        rawBody: postBody,
+                                                                        followPost: followPost)
+                                    Task {
+                                        if await viewModel.createNewThread(newThread: newThread) {
+                                            onPostCreated()
+                                        }
+                                    }
+                                }
+                            })
+                            .padding(.top, 26)
+                            .saturation(!postTitle.isEmpty && !postBody.isEmpty ? 1 : 0)
+                            Spacer()
+                        }.padding(.horizontal, 24)
+                            .frameLimit()
+                            .onRightSwipeGesture {
+                                viewModel.router.back()
+                            }
+                    }.scrollAvoidKeyboard(dismissKeyboardByTap: true)
                 }
-            }.scrollAvoidKeyboard(dismissKeyboardByTap: true)
+            }
         }
         .background(
             CoreAssets.background.swiftUIColor
