267e26995c1f638ddc9ff2809c8d317b0aa67c24
Eugene Yatsenko <eyatsenko@perpet.io>
2023-12-13 15:19:29 +0100

    chore: add support language to support urls

--- Core/Core/Configuration/Config/AgreementConfig.swift
+++ Core/Core/Configuration/Config/AgreementConfig.swift
@@ -12,6 +12,7 @@ private enum AgreementKeys: String {
     case tosURL = "TOS_URL"
     case cookiePolicyURL = "COOKIE_POLICY_URL"
     case dataSellContentURL = "DATA_SELL_CONSENT_URL"
+    case supportedLanguages = "SUPPORTED_LANGUAGES"
 }
 
 public class AgreementConfig: NSObject {
@@ -19,14 +20,44 @@ public class AgreementConfig: NSObject {
     public var tosURL: URL?
     public var cookiePolicyURL: URL?
     public var dataSellContentURL: URL?
+    public var supportedLanguages: [String]?
 
     init(dictionary: [String: AnyObject]) {
-        privacyPolicyURL = (dictionary[AgreementKeys.privacyPolicyURL.rawValue] as? String).flatMap(URL.init)
-        tosURL = (dictionary[AgreementKeys.tosURL.rawValue] as? String).flatMap(URL.init)
+        supportedLanguages = dictionary[AgreementKeys.supportedLanguages.rawValue] as? [String]
         cookiePolicyURL = (dictionary[AgreementKeys.cookiePolicyURL.rawValue] as? String).flatMap(URL.init)
         dataSellContentURL = (dictionary[AgreementKeys.dataSellContentURL.rawValue] as? String).flatMap(URL.init)
+
         super.init()
+
+        if let tosURL = dictionary[AgreementKeys.tosURL.rawValue] as? String {
+            self.tosURL = URL(string: completePath(url: tosURL))
+        }
+
+        if let privacyPolicyURL = dictionary[AgreementKeys.privacyPolicyURL.rawValue] as? String {
+            self.privacyPolicyURL = URL(string: completePath(url: privacyPolicyURL))
+        }
     }
+
+    private func completePath(url: String) -> String {
+           let langCode = Locale.current.languageCode ?? ""
+           if supportedLanguages?.contains(langCode) == false {
+               return url
+           }
+
+           let URL = URL(string: url)
+           let host = URL?.host ?? ""
+           let components = url.components(separatedBy: host)
+
+           if components.count != 2 {
+               return url
+           }
+
+           if let firstComponent = components.first, let lastComponent = components.last {
+               return "\(firstComponent)\(host)/\(langCode)\(lastComponent)"
+           }
+
+           return url
+       }
 }
 
 private let key = "AGREEMENT_URLS"
