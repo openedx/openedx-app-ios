6228e7278b7cd0a0ca6305937d14ef352bbfb269
Saeed Bashir <sbashir@edx.org>
2024-03-05 10:32:41 +0500

    fix: icon color fix

--- Authorization/Authorization/Presentation/Startup/StartupView.swift
+++ Authorization/Authorization/Presentation/Startup/StartupView.swift
@@ -26,7 +26,7 @@ public struct StartupView: View {
     public var body: some View {
         ZStack(alignment: .top) {
             VStack(alignment: .leading) {
-                ThemeAssets.appLogo.swiftUIImage.renderingMode(.template)
+                ThemeAssets.appLogo.swiftUIImage
                     .resizable()
                     .aspectRatio(contentMode: .fit)
                     .frame(maxWidth: 189, maxHeight: 89)
