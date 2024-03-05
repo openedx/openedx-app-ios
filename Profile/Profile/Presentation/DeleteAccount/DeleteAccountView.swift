f83912055537e437d60dc7b208911b4d25417bfa
forgotvas <vadim@touchappmedia.com>
2024-03-06 00:48:47 +0300

    feat: added paddings to Delete Account View

--- Profile/Profile/Presentation/DeleteAccount/DeleteAccountView.swift
+++ Profile/Profile/Presentation/DeleteAccount/DeleteAccountView.swift
@@ -19,157 +19,160 @@ public struct DeleteAccountView: View {
     }
     
     public var body: some View {
-        ZStack(alignment: .top) {
-            // MARK: - Page Body
-            ScrollView {
-                VStack {
-                    Group {
-                        ZStack {
-                            CoreAssets.bgDelete.swiftUIImage
-                            CoreAssets.deleteChar.swiftUIImage
-                                .foregroundColor(Theme.Colors.accentXColor)
-                                .offset(y: -31)
-                            CoreAssets.deleteEyes.swiftUIImage
-                                .offset(x: -7, y: -27)
-                                .accessibilityIdentifier("delete_account_image")
-                        }.padding(.top, 50)
+        GeometryReader { proxy in
+            ZStack(alignment: .top) {
+                // MARK: - Page Body
+                ScrollView {
+                    VStack {
+                        Group {
+                            ZStack {
+                                CoreAssets.bgDelete.swiftUIImage
+                                CoreAssets.deleteChar.swiftUIImage
+                                    .foregroundColor(Theme.Colors.accentXColor)
+                                    .offset(y: -31)
+                                CoreAssets.deleteEyes.swiftUIImage
+                                    .offset(x: -7, y: -27)
+                                    .accessibilityIdentifier("delete_account_image")
+                            }.padding(.top, 50)
+                            
+                            HStack {
+                                Text(ProfileLocalization.DeleteAccount.areYouSure)
+                                    .foregroundColor(Theme.Colors.navigationBarTintColor)
+                                + Text(ProfileLocalization.DeleteAccount.wantToDelete)
+                                    .foregroundColor(Theme.Colors.alert)
+                            }
+                            .accessibilityIdentifier("are_you_sure_text")
+                            
+                        }.multilineTextAlignment(.center)
+                            .font(Theme.Fonts.headlineSmall)
                         
-                        HStack {
-                            Text(ProfileLocalization.DeleteAccount.areYouSure)
-                                .foregroundColor(Theme.Colors.navigationBarTintColor)
-                            + Text(ProfileLocalization.DeleteAccount.wantToDelete)
-                                .foregroundColor(Theme.Colors.alert)
-                        }
-                        .accessibilityIdentifier("are_you_sure_text")
-
-                    }.multilineTextAlignment(.center)
-                        .font(Theme.Fonts.headlineSmall)
-                    
-                    Text(ProfileLocalization.DeleteAccount.description)
-                        .foregroundColor(Theme.Colors.textSecondary)
-                        .font(Theme.Fonts.labelLarge)
-                        .multilineTextAlignment(.center)
-                        .padding(.top, 16)
-                        .accessibilityIdentifier("delete_account_description_text")
-                    
-                    // MARK: Password
-                    Group {
-                        Text(ProfileLocalization.DeleteAccount.password)
+                        Text(ProfileLocalization.DeleteAccount.description)
                             .foregroundColor(Theme.Colors.textSecondary)
                             .font(Theme.Fonts.labelLarge)
-                            .multilineTextAlignment(.leading)
+                            .multilineTextAlignment(.center)
                             .padding(.top, 16)
-                            .accessibilityIdentifier("password_text")
+                            .accessibilityIdentifier("delete_account_description_text")
                         
-                        HStack(spacing: 11) {
-                            SecureField(ProfileLocalization.DeleteAccount.passwordDescription,
-                                        text: $viewModel.password)
+                        // MARK: Password
+                        Group {
+                            Text(ProfileLocalization.DeleteAccount.password)
+                                .foregroundColor(Theme.Colors.textSecondary)
+                                .font(Theme.Fonts.labelLarge)
+                                .multilineTextAlignment(.leading)
+                                .padding(.top, 16)
+                                .accessibilityIdentifier("password_text")
+                            
+                            HStack(spacing: 11) {
+                                SecureField(ProfileLocalization.DeleteAccount.passwordDescription,
+                                            text: $viewModel.password)
+                                .font(Theme.Fonts.labelLarge)
+                                .foregroundColor(Theme.Colors.textPrimary)
+                                .accessibilityIdentifier("password_textfield")
+                            }
+                            .padding(.horizontal, 14)
+                            .frame(minHeight: 48)
+                            .frame(maxWidth: .infinity)
+                            .background(
+                                Theme.Shapes.textInputShape
+                                    .fill(Theme.Colors.textInputBackground)
+                            )
+                            .overlay(
+                                Theme.Shapes.textInputShape
+                                    .stroke(lineWidth: 1)
+                                    .fill(Theme.Colors.textInputUnfocusedStroke)
+                            )
+                            Text(viewModel.incorrectPassword
+                                 ? ProfileLocalization.DeleteAccount.incorrectPassword
+                                 : " ")
+                            .foregroundColor(Theme.Colors.alert)
                             .font(Theme.Fonts.labelLarge)
-                            .foregroundColor(Theme.Colors.textPrimary)
-                            .accessibilityIdentifier("password_textfield")
+                            .multilineTextAlignment(.leading)
+                            .padding(.top, 0)
+                            .shake($viewModel.incorrectPassword,
+                                   onCompletion: { viewModel.incorrectPassword.toggle() })
+                            .accessibilityIdentifier("incorrect_password_text")
+                            
+                        }.frame(minWidth: 0,
+                                maxWidth: .infinity,
+                                alignment: .topLeading)
+                        
+                        // MARK: Comfirmation button
+                        if viewModel.isShowProgress {
+                            ProgressBar(size: 40, lineWidth: 8)
+                                .padding(.top, 20)
+                                .padding(.horizontal)
+                                .accessibilityIdentifier("progressbar")
+                        } else {
+                            StyledButton(ProfileLocalization.DeleteAccount.comfirm, action: {
+                                Task {
+                                    try await viewModel.deleteAccount(password: viewModel.password)
+                                }
+                            }, color: Theme.Colors.accentColor,
+                                         isActive: viewModel.password.count >= 2)
+                            .padding(.top, 18)
+                            .accessibilityIdentifier("delete_account_button")
                         }
-                        .padding(.horizontal, 14)
-                        .frame(minHeight: 48)
-                        .frame(maxWidth: .infinity)
+                        
+                        // MARK: Back to profile
+                        Button(action: {
+                            viewModel.router.back()
+                        }, label: {
+                            HStack(spacing: 9) {
+                                CoreAssets.arrowRight16.swiftUIImage.renderingMode(.template)
+                                    .rotationEffect(Angle(degrees: 180))
+                                    .foregroundColor(Theme.Colors.accentColor)
+                                Text(ProfileLocalization.DeleteAccount.backToProfile)
+                                    .font(Theme.Fonts.labelLarge)
+                                    .foregroundColor(Theme.Colors.accentColor)
+                            }
+                        })
+                        .padding(.top, 35)
+                        .accessibilityIdentifier("back_button")
+                        .frame(maxWidth: .infinity, minHeight: 42)
                         .background(
-                            Theme.Shapes.textInputShape
-                                .fill(Theme.Colors.textInputBackground)
+                            Theme.Shapes.buttonShape
+                                .fill(Theme.Colors.white)
                         )
                         .overlay(
-                            Theme.Shapes.textInputShape
-                                .stroke(lineWidth: 1)
-                                .fill(Theme.Colors.textInputUnfocusedStroke)
+                            Theme.Shapes.buttonShape
+                                .stroke(style: .init(lineWidth: 1, lineCap: .round, lineJoin: .round, miterLimit: 1))
+                                .foregroundColor(Theme.Colors.secondaryButtonBorderColor)
+                            
                         )
-                        Text(viewModel.incorrectPassword
-                             ? ProfileLocalization.DeleteAccount.incorrectPassword
-                             : " ")
-                        .foregroundColor(Theme.Colors.alert)
-                        .font(Theme.Fonts.labelLarge)
-                        .multilineTextAlignment(.leading)
-                        .padding(.top, 0)
-                        .shake($viewModel.incorrectPassword,
-                               onCompletion: { viewModel.incorrectPassword.toggle() })
-                        .accessibilityIdentifier("incorrect_password_text")
-                        
-                    }.frame(minWidth: 0,
-                            maxWidth: .infinity,
-                            alignment: .topLeading)
-                    
-                    // MARK: Comfirmation button
-                    if viewModel.isShowProgress {
-                        ProgressBar(size: 40, lineWidth: 8)
-                            .padding(.top, 20)
-                            .padding(.horizontal)
-                            .accessibilityIdentifier("progressbar")
-                    } else {
-                        StyledButton(ProfileLocalization.DeleteAccount.comfirm, action: {
-                            Task {
-                                try await viewModel.deleteAccount(password: viewModel.password)
-                            }
-                        }, color: Theme.Colors.accentColor,
-                                     isActive: viewModel.password.count >= 2)
-                        .padding(.top, 18)
-                        .accessibilityIdentifier("delete_account_button")
+                        .padding(.top, 35)
                     }
-                    
-                    // MARK: Back to profile
-                    Button(action: {
-                        viewModel.router.back()
-                    }, label: {
-                        HStack(spacing: 9) {
-                            CoreAssets.arrowRight16.swiftUIImage.renderingMode(.template)
-                                .rotationEffect(Angle(degrees: 180))
-                                .foregroundColor(Theme.Colors.accentColor)
-                            Text(ProfileLocalization.DeleteAccount.backToProfile)
-                                .font(Theme.Fonts.labelLarge)
-                                .foregroundColor(Theme.Colors.accentColor)
+                    .frameLimit(width: proxy.size.width)
+                }.padding(.horizontal, 24)
+                    .frame(minHeight: 0,
+                           maxHeight: .infinity,
+                           alignment: .top)
+                
+                    .padding(.top, 8)
+                    .navigationBarHidden(false)
+                    .navigationBarBackButtonHidden(false)
+                    .navigationTitle(ProfileLocalization.DeleteAccount.title)
+                
+                // MARK: - Error Alert
+                if viewModel.showError {
+                    VStack {
+                        Spacer()
+                        SnackBarView(message: viewModel.errorMessage)
+                    }
+                    .padding(.bottom, viewModel.connectivity.isInternetAvaliable
+                             ? 0 : OfflineSnackBarView.height)
+                    .transition(.move(edge: .bottom))
+                    .onAppear {
+                        doAfter(Theme.Timeout.snackbarMessageLongTimeout) {
+                            viewModel.errorMessage = nil
                         }
-                    })
-                    .padding(.top, 35)
-                    .accessibilityIdentifier("back_button")
-                    .frame(maxWidth: .infinity, minHeight: 42)
-                    .background(
-                        Theme.Shapes.buttonShape
-                            .fill(Theme.Colors.white)
-                    )
-                    .overlay(
-                        Theme.Shapes.buttonShape
-                            .stroke(style: .init(lineWidth: 1, lineCap: .round, lineJoin: .round, miterLimit: 1))
-                            .foregroundColor(Theme.Colors.secondaryButtonBorderColor)
-                    
-                    )
-                    .padding(.top, 35)
-                }
-            }.padding(.horizontal, 24)
-                .frame(minHeight: 0,
-                       maxHeight: .infinity,
-                       alignment: .top)
-            
-                .padding(.top, 8)
-                .navigationBarHidden(false)
-                .navigationBarBackButtonHidden(false)
-                .navigationTitle(ProfileLocalization.DeleteAccount.title)
-            
-            // MARK: - Error Alert
-            if viewModel.showError {
-                VStack {
-                    Spacer()
-                    SnackBarView(message: viewModel.errorMessage)
-                }
-                .padding(.bottom, viewModel.connectivity.isInternetAvaliable
-                         ? 0 : OfflineSnackBarView.height)
-                .transition(.move(edge: .bottom))
-                .onAppear {
-                    doAfter(Theme.Timeout.snackbarMessageLongTimeout) {
-                        viewModel.errorMessage = nil
                     }
                 }
             }
+            .background(
+                Theme.Colors.background
+                    .ignoresSafeArea()
+            )
         }
-        .background(
-            Theme.Colors.background
-                .ignoresSafeArea()
-        )
     }
 }
 
