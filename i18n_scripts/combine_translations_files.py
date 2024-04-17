c7cfac32624711defbb42423432bbc831487cbad
amr-nash <amr.nash@gmail.com>
2024-04-17 17:05:42 +0300

    make the script working with all of the languages

--- i18n_scripts/combine_translations_files.py
+++ i18n_scripts/combine_translations_files.py
@@ -1,8 +1,9 @@
 import os, localizable
+from collections import OrderedDict
 
 
 def get_modules_to_translate(modules_dir, lang='en'):
-    '''
+    """
     Retrieve the names of modules that have translation files for a specified language.
 
     Parameters:
@@ -14,36 +15,21 @@ def get_modules_to_translate(modules_dir, lang='en'):
 
     Logic:
         1. List all directories in the modules directory, excluding the 'I18N' directory.
-        2. Iterate over each directory in the list:
-            a. Check if the directory contains a subdirectory with the same name.
-            b. Check if the subdirectory contains a subdirectory for the specified language.
-            c. Check if the language directory contains a file named 'Localizable.strings'.
-            d. If all conditions are met, add the module name to the list of modules to translate.
-        3. Return the list of module names.
-    '''
-    # Step 1: List all directories in modules_dir excluding 'I18N'
-    dirs = [file for file in os.listdir(modules_dir) if
-            os.path.isdir(os.path.join(modules_dir, file)) and file != 'I18N']
+        2. Return the list of module names.
+    """
+    dirs = [directory for directory in os.listdir(modules_dir) if
+            os.path.isdir(os.path.join(modules_dir, directory)) and directory != 'I18N']
 
     modules_list = []
-    # Step 2: Iterate over each directory in the list
     for module in dirs:
-        module_path = os.path.join(modules_dir, module)
-        # Step 2a: Check if the directory contains a subdirectory with the same name
-        if module in os.listdir(module_path):
-            lang_dir = os.path.join(module_path, module, f'{lang}.lproj')
-            # Step 2b: Check if the subdirectory contains a subdirectory for the specified language
-            if os.path.isdir(lang_dir):
-                # Step 2c: Check if the language directory contains 'Localizable.strings' file
-                if 'Localizable.strings' in os.listdir(lang_dir):
-                    # Step 2d: Add the module name to the list of modules to translate
-                    modules_list.append(module)
-    # Step 3: Return the list of module names
+        translation_path = os.path.join(modules_dir, module, module, f'{lang}.lproj', 'Localizable.strings')
+        if os.path.exists(translation_path) and os.path.isfile(translation_path):
+            modules_list.append(module)
     return modules_list
 
 
-def get_translations_list(modules_dir, modules, lang='en'):
-    '''
+def get_translations_ordereddict(modules_dir, lang='en'):
+    """
     Retrieve the list of translations from all specified modules.
 
     Parameters:
@@ -52,17 +38,19 @@ def get_translations_list(modules_dir, modules, lang='en'):
         lang (str): The language code for which translations are sought. Defaults to 'en'.
 
     Returns:
-        list of dict: A list of dictionaries containing the 'key', 'value', and 'comment' for each translation line.
+        OrderedDict of dict: An ordered dict of dictionaries containing the 'key', 'value', and 'comment' for each
+        translation line. The key of the OrderedDict is the value of the key in the contained dict in addition to the
+        name of the module containing the translation
 
     Logic:
         1. Iterate over each module in the list of modules:
             a. Construct the path to the Localizable.strings file for the module in the specified language.
             b. Parse the Localizable.strings file to retrieve translations.
             c. Add the module's name to each translation key to distinguish it from other modules.
-        2. Return the list of translations.
-    '''
-    translations = []
-
+        2. Return the OrderedDict of translations.
+    """
+    ordered_dict_of_translations = OrderedDict()
+    modules = get_modules_to_translate(modules_dir, lang=lang)
     # Step 1: Iterate over each module in the list of modules
     for module in modules:
         # Step 1a: Construct the path to the Localizable.strings file
@@ -73,13 +61,10 @@ def get_translations_list(modules_dir, modules, lang='en'):
 
         # Step 1c: Add the module's name to each translation key
         for line in module_translations:
-            line['key'] = f"{module.upper()}.{line['key']}"
-
-        # Concatenate the module translations to the list of all translations
-        translations += module_translations
+            ordered_dict_of_translations[f"{module.upper()}.{line['key']}"] = line
 
     # Step 2: Return the list of translations
-    return translations
+    return ordered_dict_of_translations
 
 
 def _escape(s):
@@ -88,22 +73,39 @@ def _escape(s):
     return s
 
 
-def write_dict_to_file(content_list, modules_dir, lang='en'):
+def write_dict_to_file(content_ordered_dict, modules_dir, lang='en'):
     translation_file = os.path.join(modules_dir, 'I18N', 'I18N', f'{lang}.lproj')
     os.makedirs(translation_file, exist_ok=True)
     with open(os.path.join(translation_file, 'Localizable.strings'), 'w') as f:
-        for line_dict in content_list:
-            comment = line_dict.get('comment', '')  # Retrieve the comment, if present
+        for key, value in content_ordered_dict.items():
+            comment = value.get('comment', '')  # Retrieve the comment, if present
             if comment:
                 f.write(f"/* {comment} */\n")
-            f.write(f'"{line_dict.get("key")}" = "{_escape(line_dict["value"])}";\n')
+            f.write(f'"{key}" = "{_escape(value["value"])}";\n')
+
+
+def get_languages(module_dir):
+    languages = set()
+    modules = [module for module in os.listdir(module_dir) if os.path.exists(os.path.join(module_dir, module, module))]
+    for module in modules:
+        if os.path.isdir(os.path.join(module_dir, module, module)):
+            lang_list = [elem.strip('.lproj') for elem in os.listdir(os.path.join(module_dir, module, module)) if elem.endswith('.lproj')]
+            languages.update(lang_list)
+    return languages
 
 
 if __name__ == "__main__":
     modules_dir = os.path.abspath(os.path.join(os.path.dirname(__file__), ".."))
-    modules = get_modules_to_translate(modules_dir)
-    translation_list = get_translations_list(modules_dir, modules)
-    write_dict_to_file(translation_list, modules_dir)
+    langs = get_languages(modules_dir)
+    langs.discard('en')
+    en_translation_ordered_dict = get_translations_ordereddict(modules_dir, lang='en')
+    write_dict_to_file(en_translation_ordered_dict, modules_dir, lang='en')
+    for lang in langs:
+        lang_translation_ordered_dict = get_translations_ordereddict(modules_dir, lang=lang)
+        in_en_not_in_lang = OrderedDict([(key, value) for key, value in en_translation_ordered_dict.items()
+                                         if key not in lang_translation_ordered_dict])
+        lang_translation_ordered_dict.update(in_en_not_in_lang)
+        write_dict_to_file(lang_translation_ordered_dict, modules_dir, lang=lang)
 
 
 def make_test_dirs_and_files():
