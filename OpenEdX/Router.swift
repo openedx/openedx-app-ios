8c65f45ea90fdfc933aa746ea12356f20eb155b3
stepanokdev <ivan.stepanok@raccoongang.com>
2024-06-07 14:57:05 +0300

    fix: address feedback

--- OpenEdX/Router.swift
+++ OpenEdX/Router.swift
@@ -595,7 +595,7 @@ public class Router: AuthorizationRouter,
         let config = Container.shared.resolve(ConfigProtocol.self)!
         let courseDropDownNavigationEnabled = config.uiComponents.courseDropDownNavigationEnabled
 
-        if !courseDropDownNavigationEnabled || currentCourseTabSelection == CourseTab.dates.rawValue {
+        if courseDropDownNavigationEnabled || currentCourseTabSelection == CourseTab.dates.rawValue {
             controllers.removeLast(1)
             controllers.append(contentsOf: [controllerUnit])
         } else {
