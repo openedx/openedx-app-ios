06f4db1e1214a96f42bd70664acc4a911750a3b2
Vadim Kuznetsov <vadim@touchappmedia.com>
2024-06-25 20:46:46 +0300

    fix: crash with CoreData

--- OpenEdX/Data/DiscoveryPersistence.swift
+++ OpenEdX/Data/DiscoveryPersistence.swift
@@ -19,25 +19,27 @@ public class DiscoveryPersistence: DiscoveryPersistenceProtocol {
     }
     
     public func loadDiscovery() throws -> [CourseItem] {
-        let result = try? context.fetch(CDDiscoveryCourse.fetchRequest())
-            .map { CourseItem(name: $0.name ?? "",
-                              org: $0.org ?? "",
-                              shortDescription: $0.desc ?? "",
-                              imageURL: $0.imageURL ?? "",
-                              hasAccess: $0.hasAccess,
-                              courseStart: $0.courseStart,
-                              courseEnd: $0.courseEnd,
-                              enrollmentStart: $0.enrollmentStart,
-                              enrollmentEnd: $0.enrollmentEnd,
-                              courseID: $0.courseID ?? "",
-                              numPages: Int($0.numPages),
-                              coursesCount: Int($0.courseCount),
-                              progressEarned: 0,
-                              progressPossible: 0)}
-        if let result, !result.isEmpty {
-            return result
-        } else {
-            throw NoCachedDataError()
+        try context.performAndWait {
+            let result = try? context.fetch(CDDiscoveryCourse.fetchRequest())
+                .map { CourseItem(name: $0.name ?? "",
+                                  org: $0.org ?? "",
+                                  shortDescription: $0.desc ?? "",
+                                  imageURL: $0.imageURL ?? "",
+                                  hasAccess: $0.hasAccess,
+                                  courseStart: $0.courseStart,
+                                  courseEnd: $0.courseEnd,
+                                  enrollmentStart: $0.enrollmentStart,
+                                  enrollmentEnd: $0.enrollmentEnd,
+                                  courseID: $0.courseID ?? "",
+                                  numPages: Int($0.numPages),
+                                  coursesCount: Int($0.courseCount),
+                                  progressEarned: 0,
+                                  progressPossible: 0)}
+            if let result, !result.isEmpty {
+                return result
+            } else {
+                throw NoCachedDataError()
+            }
         }
     }
     
@@ -68,23 +70,25 @@ public class DiscoveryPersistence: DiscoveryPersistenceProtocol {
     }
     
     public func loadCourseDetails(courseID: String) throws -> CourseDetails {
-        let request = CDCourseDetails.fetchRequest()
-        request.predicate = NSPredicate(format: "courseID = %@", courseID)
-        guard let courseDetails = try? context.fetch(request).first else { throw NoCachedDataError() }
-        return CourseDetails(
-            courseID: courseDetails.courseID ?? "",
-            org: courseDetails.org ?? "",
-            courseTitle: courseDetails.courseTitle ?? "",
-            courseDescription: courseDetails.courseDescription ?? "",
-            courseStart: courseDetails.courseStart,
-            courseEnd: courseDetails.courseEnd,
-            enrollmentStart: courseDetails.enrollmentStart,
-            enrollmentEnd: courseDetails.enrollmentEnd,
-            isEnrolled: courseDetails.isEnrolled,
-            overviewHTML: courseDetails.overviewHTML ?? "",
-            courseBannerURL: courseDetails.courseBannerURL ?? "",
-            courseVideoURL: nil
-        )
+        try context.performAndWait {
+            let request = CDCourseDetails.fetchRequest()
+            request.predicate = NSPredicate(format: "courseID = %@", courseID)
+            guard let courseDetails = try? context.fetch(request).first else { throw NoCachedDataError() }
+            return CourseDetails(
+                courseID: courseDetails.courseID ?? "",
+                org: courseDetails.org ?? "",
+                courseTitle: courseDetails.courseTitle ?? "",
+                courseDescription: courseDetails.courseDescription ?? "",
+                courseStart: courseDetails.courseStart,
+                courseEnd: courseDetails.courseEnd,
+                enrollmentStart: courseDetails.enrollmentStart,
+                enrollmentEnd: courseDetails.enrollmentEnd,
+                isEnrolled: courseDetails.isEnrolled,
+                overviewHTML: courseDetails.overviewHTML ?? "",
+                courseBannerURL: courseDetails.courseBannerURL ?? "",
+                courseVideoURL: nil
+            )
+        }
     }
     
     public func saveCourseDetails(course: CourseDetails) {
