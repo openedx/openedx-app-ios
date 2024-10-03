ac3dbe5a972b477cf89e4318af384346a3c18708
Saeed Bashir <sbashir@2u.com>
2024-10-03 10:25:00 +0500

    chore: update tab bar icons for selected unselected states (#81)

--- OpenEdX/View/MainScreenView.swift
+++ OpenEdX/View/MainScreenView.swift
@@ -96,7 +96,11 @@ struct MainScreenView: View {
                     registerBanner
                 }
                 .tabItem {
-                    CoreAssets.learn.swiftUIImage.renderingMode(.template)
+                    if viewModel.selection == .dashboard {
+                        CoreAssets.learnActive.swiftUIImage.renderingMode(.template)
+                    } else {
+                        CoreAssets.learnInactive.swiftUIImage.renderingMode(.template)
+                    }
                     Text(CoreLocalization.Mainscreen.learn)
                 }
                 .tag(MainTab.dashboard)
@@ -126,7 +130,11 @@ struct MainScreenView: View {
                     }
                 }
                 .tabItem {
-                    CoreAssets.discovery.swiftUIImage.renderingMode(.template)
+                    if viewModel.selection == .discovery {
+                        CoreAssets.discoverActive.swiftUIImage.renderingMode(.template)
+                    } else {
+                        CoreAssets.discoverInactive.swiftUIImage.renderingMode(.template)
+                    }
                     Text(CoreLocalization.Mainscreen.discovery)
                 }
                 .tag(MainTab.discovery)
@@ -139,7 +147,11 @@ struct MainScreenView: View {
                 )
             }
             .tabItem {
-                CoreAssets.profile.swiftUIImage.renderingMode(.template)
+                if viewModel.selection == .profile {
+                    CoreAssets.profileActive.swiftUIImage.renderingMode(.template)
+                } else {
+                    CoreAssets.profileInactive.swiftUIImage.renderingMode(.template)
+                }
                 Text(CoreLocalization.Mainscreen.profile)
             }
             .tag(MainTab.profile)
