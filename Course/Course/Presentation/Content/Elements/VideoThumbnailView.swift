fce59afd7bbcfdb6651a4ef30fed4785505a5987
IvanStepanok <ivan.stepanok@raccoongang.com>
2025-07-30 18:40:22 +0300

    feat: move thumbnail logic to external service

--- Course/Course/Presentation/Content/Elements/VideoThumbnailView.swift
+++ Course/Course/Presentation/Content/Elements/VideoThumbnailView.swift
@@ -10,6 +10,7 @@ import Core
 import Theme
 import Kingfisher
 import AVFoundation
+import Swinject
 
 struct VideoThumbnailView: View {
     
@@ -28,6 +29,10 @@ struct VideoThumbnailView: View {
     
     @State private var thumbnailImage: UIImage?
     @State private var isGeneratingThumbnail = false
+    
+    private var thumbnailService: VideoThumbnailServiceProtocol {
+        Container.shared.resolve(VideoThumbnailServiceProtocol.self)!
+    }
     
     private var thumbnailURL: URL? {
         // First priority: YouTube thumbnail
@@ -212,76 +217,15 @@ struct VideoThumbnailView: View {
     }
     
     private func generateVideoThumbnailIfNeeded(from url: URL) async {
-        let cacheKey = "video_thumbnail_\(url.absoluteString.hash)"
-        
-        // Check if thumbnail is already cached (both memory and disk)
-        do {
-            let cacheResult = try await ImageCache.default.retrieveImage(forKey: cacheKey)
-            if let cachedImage = cacheResult.image {
-                await MainActor.run {
-                    self.thumbnailImage = cachedImage
-                }
-                return
-            }
-        } catch {
-            // Cache retrieval failed, continue to generate new thumbnail
-        }
-        
         await MainActor.run {
             self.isGeneratingThumbnail = true
         }
         
-        do {
-            let image = try await generateVideoThumbnail(from: url)
-            
-            // Cache the generated thumbnail to both memory and disk
-            try await ImageCache.default.store(
-                image,
-                forKey: cacheKey,
-                toDisk: true
-            )
-            
-            await MainActor.run {
-                self.thumbnailImage = image
-                self.isGeneratingThumbnail = false
-            }
-        } catch {
-            await MainActor.run {
-                self.isGeneratingThumbnail = false
-            }
-        }
-    }
-    
-    private func generateVideoThumbnail(from url: URL) async throws -> UIImage {
-        let asset = AVAsset(url: url)
-        let imageGenerator = AVAssetImageGenerator(asset: asset)
-        imageGenerator.appliesPreferredTrackTransform = true
-        imageGenerator.requestedTimeToleranceBefore = .zero
-        imageGenerator.requestedTimeToleranceAfter = .zero
+        let image = await thumbnailService.generateVideoThumbnailIfNeeded(from: url)
         
-        let time = CMTime(seconds: 1.0, preferredTimescale: 600)
-        
-        return try await withCheckedThrowingContinuation { continuation in
-            imageGenerator.generateCGImagesAsynchronously(
-                forTimes: [NSValue(time: time)]) { _, cgImage, _, _, error in
-                if let error = error {
-                    continuation.resume(throwing: error)
-                    return
-                }
-                
-                guard let cgImage = cgImage else {
-                    continuation.resume(
-                        throwing: NSError(
-                            domain: "VideoThumbnailError", code: -1,
-                            userInfo: [NSLocalizedDescriptionKey: "Failed to generate thumbnail"]
-                        )
-                    )
-                    return
-                }
-                
-                let image = UIImage(cgImage: cgImage)
-                continuation.resume(returning: image)
-            }
+        await MainActor.run {
+            self.thumbnailImage = image
+            self.isGeneratingThumbnail = false
         }
     }
     
