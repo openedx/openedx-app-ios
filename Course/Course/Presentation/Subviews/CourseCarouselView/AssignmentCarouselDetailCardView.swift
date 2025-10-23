2a9ced6274316c5d297ffe6fcd27fc80c7b71a4b
DemianRaccoonGang <demian.yushyn@raccoongang.com>
2025-10-23 11:24:14 +0300

    fix: MP4 video format, assignments dates

--- Course/Course/Presentation/Subviews/CourseCarouselView/AssignmentCarouselDetailCardView.swift
+++ Course/Course/Presentation/Subviews/CourseCarouselView/AssignmentCarouselDetailCardView.swift
@@ -3,39 +3,43 @@ import Theme
 import Core
 
 struct AssignmentCarouselDetailCardView: View {
-    
+
     let detailData: AssignmentDetailData
-    
+
     private var subsectionUI: CourseProgressSubsectionUI {
         detailData.subsectionUI
     }
-    
+
     private var sectionName: String {
         subsectionUI.sectionName
     }
-    
+
     private var status: AssignmentCardStatus {
         return subsectionUI.status
     }
-    
+
     private var statusText: String {
         return subsectionUI.statusTextForCarousel
     }
-    
+
     var body: some View {
         Button(action: {
             detailData.onAssignmentTap(subsectionUI)
         }) {
             HStack {
                 VStack(alignment: .leading, spacing: 8) {
-                    if detailData.subsectionUI.status != .completed && detailData.subsectionUI.date != nil {
-                        HStack {
-                            Text(CourseLocalization.CourseCarousel.nextAssignments)
-                                .font(Theme.Fonts.titleMedium)
-                                .foregroundStyle(Theme.Colors.textPrimary)
+                    HStack {
+                        
+                        if detailData.subsectionUI.status == .pastDue {
+                            CoreAssets.icAssignmentPastDue.swiftUIImage
                         }
+
+                        Text(detailData.subsectionUI.status == .pastDue ? "Past Due"
+                             : CourseLocalization.CourseCarousel.nextAssignments)
+                        .font(Theme.Fonts.titleMedium)
+                        .foregroundStyle(Theme.Colors.textPrimary)
                     }
-                    
+
                     HStack {
                         VStack(alignment: .leading) {
                             if detailData.subsectionUI.date != nil {
@@ -43,18 +47,18 @@ struct AssignmentCarouselDetailCardView: View {
                                     .font(Theme.Fonts.labelSmall)
                                     .foregroundColor(Theme.Colors.accentColor)
                             }
-                            
+
                             Text(subsectionUI.subsection.displayName)
                                 .font(Theme.Fonts.titleSmall)
                                 .foregroundColor(Theme.Colors.textPrimary)
-                            
+
                             Text(sectionName)
                                 .font(Theme.Fonts.labelSmall)
                                 .foregroundColor(Theme.Colors.textSecondaryDark)
                         }
-                        
+
                         Spacer()
-                        
+
                         CoreAssets.chevronRight.swiftUIImage
                             .resizable()
                             .renderingMode(.template)
